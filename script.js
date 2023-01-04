8dffbf7a9f21abb60215c5a9bf7432c5b1dd8b7b
ui-anmol <ui.anmol94@gmail.com>
2023-01-04 11:09:44 +0100

    wip

---
+++ script.js
@@ -0,0 +1,16 @@
+const tiles = Array.from(document.getElementsByClassName('tiles'));
+const tilesContent = Array.from(document.getElementsByClassName('tiles-content'));
+function tileSelection (event) {
+    if(!event.target.classList.contains("active-tile")) {
+        tiles.forEach(e => e.classList.remove("active-tile"));
+        event.target.classList.add("active-tile")
+    }
+    tilesContent.forEach( ele => {
+        ele.classList.remove("tiles-content-active");
+        if(ele.getAttribute("data-display") == event.target.innerText) {
+            ele.classList.add("tiles-content-active")
+        }
+       
+    })
+    
+}
