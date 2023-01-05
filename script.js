8197741922379fcafc23c26fcf6e638331570122
ui-anmol <ui.anmol94@gmail.com>
2023-01-05 13:12:01 +0100

    optimized the function

--- script.js
+++ script.js
@@ -1,17 +1,15 @@
 const tiles = Array.from(document.getElementsByClassName('tiles'));
 const tilesContent = Array.from(document.getElementsByClassName('tiles-content'));
 function tileSelection (event) {
-    console.log()
     if(!event.target.classList.contains("active-tile") && event.target.classList.contains("tiles")) {
         tiles.forEach(e => e.classList.remove("active-tile"));
         event.target.classList.add("active-tile")
-    }
-    tilesContent.forEach( ele => {
-        ele.classList.remove("tiles-content-active");
-        if(ele.getAttribute("data-display") == event.target.innerText) {
-            ele.classList.add("tiles-content-active")
-        }
-       
-    })
-    
+        tilesContent.forEach( ele => {
+            ele.classList.remove("tiles-content-active");
+            if(ele.getAttribute("data-display") == event.target.innerText) {
+                ele.classList.add("tiles-content-active")
+            }
+           
+        })
+    }  
 }
