0420acc0422398679e0387f61650a793a698a12b
ui-anmol <ui.anmol94@gmail.com>
2023-01-16 15:26:41 +0100

    test

--- script.js
+++ script.js
@@ -13,3 +13,48 @@ function tileSelection (event) {
         })
     }  
 }
+// Try edit me
+let inputObj = {
+  frames: [
+    {
+      first: 5,
+      second: 4
+    },
+    {
+      first: 10,
+      second: 0
+    },
+    {
+      first: 0,
+      second: 10
+    }
+  ]
+};
+function calculateScore(args) {
+  let totalScore = 0;
+  let dummyScore = {
+    first: 0,
+    second: 0,
+    status: "Open",
+    bonus: 0
+  };
+  let frames = [dummyScore, ...args.frames];
+  for (let i = 0; i <= frames.length - 1; i++) {
+    if (frames[i].first === 10) {
+      frames[i].score = frames[i].first + frames[i].second;
+      frames[i].status = "Strike";
+      totalScore = totalScore + frames[i].score + frames[i - 1].bonus;
+    } else if (frames[i].first + frames[i.second === 10]) {
+      frames[i].score = frames[i].first;
+      frames[i].status = "Spare";
+      totalScore = totalScore + frames[i].score + frames[i - 1].bonus;
+    } else {
+      frames[i].score = frames[i].first + frames[i].second;
+      frames[i].status = "Open";
+      totalScore = totalScore + frames[i].score + frames[i - 1].bonus;
+    }
+  }
+}
+console.log(calculateScore(inputObj));
+
+
